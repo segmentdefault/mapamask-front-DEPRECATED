156bf39eeb5d86cf5b4f594992011ac2b93130fa
Adrian Serrano Castillo <adrian@arkerlabs.com>
2022-06-02 18:35:34 +0200

    fix: 🚀API_URL corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: "https://mapamask.heroku.com/api/"
+  API_URL: "https://mapamask.herokuapp.com/api/"
   //API_URL: "http://localhost:3000/api/"
 };
