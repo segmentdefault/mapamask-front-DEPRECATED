0140a3a869330857cf2d9bbc29bb31744042d087
Adrian Serrano Castillo <adrianserranocontacto@gmail.com>
2024-07-17 16:58:30 +0200

    chore: update backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: "https://mapamask.herokuapp.com/api/"
+  API_URL: "https://mapamask-back.onrender.com/api/"
   //API_URL: "http://localhost:3000/api/"
 };
 
